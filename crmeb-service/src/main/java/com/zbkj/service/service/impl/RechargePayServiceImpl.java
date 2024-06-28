0842c69a0741b888b65f94a9c5d1b3ae51296993
wyb <2429468825@qq.com>
2024-06-28 15:45:07 +0800

    小程序发货信息录入-余额虚拟发货

--- crmeb-service/src/main/java/com/zbkj/service/service/impl/RechargePayServiceImpl.java
+++ crmeb-service/src/main/java/com/zbkj/service/service/impl/RechargePayServiceImpl.java
@@ -1,20 +1,22 @@
 package com.zbkj.service.service.impl;
 
 import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson.JSONObject;
 import com.zbkj.common.constants.Constants;
+import com.zbkj.common.constants.WeChatConstants;
 import com.zbkj.common.utils.DateUtil;
 import com.zbkj.common.model.finance.UserRecharge;
 import com.zbkj.common.model.user.User;
 import com.zbkj.common.model.user.UserBill;
-import com.zbkj.service.service.RechargePayService;
-import com.zbkj.service.service.UserBillService;
-import com.zbkj.service.service.UserRechargeService;
-import com.zbkj.service.service.UserService;
+import com.zbkj.common.utils.RestTemplateUtil;
+import com.zbkj.service.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -36,6 +38,12 @@ public class RechargePayServiceImpl implements RechargePayService {
     private UserRechargeService userRechargeService;
 
     @Autowired
+    private SystemConfigService systemConfigService;
+
+    @Autowired
+    private UserTokenService userTokenService;
+
+    @Autowired
     private UserService userService;
 
     @Autowired
@@ -43,6 +51,12 @@ public class RechargePayServiceImpl implements RechargePayService {
 
     @Autowired
     private UserBillService userBillService;
+
+    @Autowired
+    private RestTemplateUtil restTemplateUtil;
+
+    @Autowired
+    private WechatNewService wechatNewService;
 
     /**
      * 支付成功处理
@@ -68,6 +82,36 @@ public class RechargePayServiceImpl implements RechargePayService {
         userBill.setType(Constants.USER_BILL_TYPE_PAY_RECHARGE);
         userBill.setNumber(payPrice);
         userBill.setBalance(balance);
+        // TODO: 微信发货
+        JSONObject jsonObject = new JSONObject();
+        JSONObject orderKey = new JSONObject();
+        JSONObject shipping = new JSONObject();
+        JSONObject payer = new JSONObject();
+        List<JSONObject> shippingList = new ArrayList<>();
+
+        // 订单单号类型，用于确认需要上传详情的订单。枚举值1，使用下单商户号和商户侧单号；枚举值2，使用微信支付单号。
+        orderKey.put("order_number_type", "1");
+        orderKey.put("mchid", systemConfigService.getValueByKeyException(Constants.CONFIG_KEY_PAY_WE_CHAT_MCH_ID));
+        // 商户系统内部订单号，只能是数字、大小写字母`_-*`且在同一个商户号下唯一
+        orderKey.put("out_trade_no", userRecharge.getOrderId());
+
+        jsonObject.put("order_key", orderKey);
+        // 1、实体物流配送采用快递公司进行实体物流配送形式 2、同城配送 3、虚拟商品，虚拟商品，例如话费充值，点卡等，无实体配送形式 4、用户自提
+        jsonObject.put("logistics_type", "3");
+        // 发货模式，发货模式枚举值：1、UNIFIED_DELIVERY（统一发货）2、SPLIT_DELIVERY（分拆发货） 示例值: UNIFIED_DELIVERY
+        jsonObject.put("delivery_mode", "1");
+
+        shipping.put("item_desc", "余额充值-虚拟发货");
+        jsonObject.put("shipping_list", shippingList);
+        jsonObject.put("upload_time", DateUtil.nowDateTime("YYYY-MM-DDThh:mm:ss.sss±hh:mm"));
+
+        payer.put("openid", userTokenService.getTokenByUserId(userRecharge.getUid() ,2));
+        jsonObject.put("payer", payer);
+        System.out.println("6.28 余额虚拟发货:" + jsonObject.toJSONString());
+        // 虚拟发货
+        String url = StrUtil.format(WeChatConstants.WECHAT_SHIPMENT_API_URL, wechatNewService.getMiniAccessToken());
+        String s = restTemplateUtil.postJsonData(url, new JSONObject());
+        System.out.println("6.28 余额虚拟发货返回结果:" + s);
         userBill.setMark(StrUtil.format("余额增加了{}元", payPrice));
         userBill.setStatus(1);
         userBill.setCreateTime(DateUtil.nowDateTime());
